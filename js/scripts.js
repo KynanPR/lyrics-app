f711035d93fa4bea681a9c16b80001b2d335be4f
KynanPR <kynanpr@gmail.com>
2022-05-24 16:43:43 +0100

    Clear scripts.js ready to start fresh

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-let myName = 'Kynan';
-alert(myName);
-
-let favouriteFood = 'Dunno really. This question is too profound to have the answer fit into a simple JS var';
-document.write(favouriteFood);
