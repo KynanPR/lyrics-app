2b1543061e5900b82ed9a08e6b4afc76e36194b6
KynanPR <kynanpr@gmail.com>
2022-05-26 14:04:39 +0100

    Write Pokemon names and heights to DOM
    Comment on cuteness of small Pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -33,3 +33,26 @@ pokemonList.push(
     types: [pokemonTypes[1]],
   }
 );
+
+// Loop through pokemonList writing name and height to DOM. Calls them cute if shorter than 0.5
+for (let i = 0, writeText = '', heightComment = ''; i < pokemonList.length; i++) {
+  // Simpler local vars
+  let name = pokemonList[i].name;
+  let height = pokemonList[i].height;
+
+  // Create text to write. Only add comment to end of height if <= 0.5
+  if (height <= 0.5) {
+    heightComment = '<i> - So small and cute!</i>';
+  }
+
+  writeText = `<p>
+  <b>Pokemon ${i + 1}'s Name:</b> ${name}<br>
+  <b>Pokemon ${i + 1}'s Height:</b> ${height}m${heightComment}
+  </p>`;
+
+  // Write text to DOM
+  document.write(writeText);
+
+  // Clear height comment for next iteration
+  heightComment = '';
+}
