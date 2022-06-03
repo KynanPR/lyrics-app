3654a8baa8c6ce73ea085c887aca6039ff3343f3
KynanPR <kynanpr@gmail.com>
2022-06-03 14:15:35 +0100

    Replaced for loop with functional forEach
    Same effect/output. But written in a functional style and using forEach rather than a custom for loop

--- js/scripts.js
+++ js/scripts.js
@@ -34,25 +34,23 @@ pokemonList.push(
   }
 );
 
-// Loop through pokemonList writing name and height to DOM. Calls them cute if shorter than 0.5
-for (let i = 0, writeText = '', heightComment = ''; i < pokemonList.length; i++) {
-  // Simpler local vars
-  let name = pokemonList[i].name;
-  let height = pokemonList[i].height;
+function getCuteMessage(height) {
+  return height <= 0.5 ? '<i> - So small and cute!</i>' : '';
+}
 
-  // Create text to write. Only add comment to end of height if <= 0.5
-  if (height <= 0.5) {
-    heightComment = '<i> - So small and cute!</i>';
-  }
+function writePokemon(pokemon, i) {
+  // Simpler local vars
+  let name = pokemon.name;
+  let height = pokemon.height;
+  let cuteMessage = getCuteMessage(height);
 
   writeText = `<p>
   <b>Pokemon ${i + 1}'s Name:</b> ${name}<br>
-  <b>Pokemon ${i + 1}'s Height:</b> ${height}m${heightComment}
+  <b>Pokemon ${i + 1}'s Height:</b> ${height}m${cuteMessage}
   </p>`;
 
   // Write text to DOM
   document.write(writeText);
-
-  // Clear height comment for next iteration
-  heightComment = '';
 }
+
+pokemonList.forEach(writePokemon);
