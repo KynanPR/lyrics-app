75414b69614f8a49c76f19208fba093169b91418
KynanPR <kynanpr@gmail.com>
2022-06-14 15:42:15 +0100

    Validation for Pokemon objects - NOT WORKING
    Not working at the moment

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,13 @@ const pokemonTypes = [
   'Poison',
 ];
 
+// Create template Pokemon object
+const pokemonTemplate = {
+  name: 'String', // name as string
+  height: 1, // height as float
+  types: ['Array', 'Of', 'Strings'], // types as an array of at strings - length >= 1
+};
+
 // Setup IIFE to protect pokemonList
 // Create pokemonRepository object with properies of add & getAll functions
 let pokemonRepository = (function() {
@@ -13,7 +20,12 @@ let pokemonRepository = (function() {
 
   // Create function to add pokemon to pokemonList
   function add(pokemon) {
-    pokemonList.push(pokemon);
+    // Validate argument. If valid add to pokemonList. If invalid show warning in console
+    if (isObject(pokemon) && isValidPokemon(pokemon)) {
+      pokemonList.push(pokemon);
+    } else {
+      console.warn('Invalid Pokemon object - Ensure all keys are present and correct');
+    }
   }
 
   // Create function that returns pokemonList
@@ -29,6 +41,27 @@ let pokemonRepository = (function() {
     getAll: getAll,
   };
 })();
+
+// Pokemon validation functions
+// Check if object
+function isObject(varValidating) {
+  console.log(typeof varValidating);
+  return (typeof varValidating === 'object' ? true : false);
+}
+
+// Check object keys are as expected
+function isValidPokemon(validatingPokemon) {
+  // Create arrays of each of the objects keys
+  const templatePokemonKeys = Object.keys(pokemonTemplate);
+  console.log(templatePokemonKeys);
+  const validatingPokemonKeys = Object.keys(validatingPokemon);
+  console.log(validatingPokemonKeys);
+
+  validatingPokemonKeys == templatePokemonKeys ? console.log('True') : console.log('False');
+
+  // Only checks for exact match of keys, so order matters - Possibly IMPROVE
+  return (validatingPokemonKeys == templatePokemonKeys ? true : false);
+}
 
 pokemonRepository.add({
   name: 'Charmander',
